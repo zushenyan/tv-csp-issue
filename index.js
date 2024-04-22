2d9dbafe0d50c1e341f7aaf0d662d9b0fa396a9c
Andrew Yan <zushenyan@gmail.com>
2024-04-22 14:47:31 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const path = require('path')
+const cors = require('cors')
 const app = express()
 
-app.use(express.static('public', {
-  setHeaders: (res) => {
-    res.set('Content-Security-Policy', "script-src 'self' 'unsafe-inline' 'nonce-abc'; frame-src 'self'")
-  }
-}))
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.set('Content-Security-Policy', "script-src 'self' 'unsafe-inline' 'nonce-abc'; frame-src 'self' blob:")
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.use(express.static('public'))
 
 app.listen(3000)
